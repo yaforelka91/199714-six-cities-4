aa4e5a83e77f6d9b32e7591135e15095c82275ca
Maria Izvekova <yaforelka@gmail.com>
2020-06-12 10:59:14 +0300

    add propTypes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './components/app/app.jsx';
 const init = () => {
   const offersData = {
     offersCount: 312,
+    offersList: [`Beautiful & luxurious apartment at great location`,
+      `Wood and stone place`, `Canal View Prinsengracht`,
+      `Nice, cozy, warm big bed apartment`, `Wood and stone place`]
   };
 
   reactDOM.render(
-      <App offersCount={offersData.offersCount} />,
+      <App offersCount={offersData.offersCount} offersList={offersData.offersList} />,
       document.querySelector(`#root`)
   );
 };
