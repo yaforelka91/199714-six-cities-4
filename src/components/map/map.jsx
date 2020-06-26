429a7eeee47622ca8f2462e61d90205f76c7df65
Maria Izvekova <yaforelka@gmail.com>
2020-06-26 08:46:01 +0300

    add statement & fix unmount

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -12,41 +12,45 @@ class Map extends PureComponent {
       currentCoords: [52.38333, 4.9],
       zoom: 12,
     };
+
+    this.mapObject = null;
   }
 
   componentDidMount() {
     const mapElement = this._mapRef.current;
     const {offers} = this.props;
 
-    const mapObject = leaflet.map(mapElement, {
-      center: this.state.currentCoords,
-      zoom: this.state.zoom,
-      zoomControl: false,
-      marker: true
-    });
+    if (mapElement) {
+      this.mapObject = leaflet.map(mapElement, {
+        center: this.state.currentCoords,
+        zoom: this.state.zoom,
+        zoomControl: false,
+        marker: true
+      });
 
-    mapObject.setView(this.state.currentCoords, this.state.zoom);
+      this.mapObject.setView(this.state.currentCoords, this.state.zoom);
 
-    leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    })
-      .addTo(mapObject);
+      leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
+        .addTo(this.mapObject);
 
-    const icon = leaflet.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [27, 39]
-    });
+      const icon = leaflet.icon({
+        iconUrl: `img/pin.svg`,
+        iconSize: [27, 39]
+      });
 
-    offers.forEach((offer) => {
-      leaflet
-      .marker(offer.coords, {icon})
-      .addTo(mapObject);
-    });
+      offers.forEach((offer) => {
+        leaflet
+        .marker(offer.coords, {icon})
+        .addTo(this.mapObject);
+      });
+    }
   }
 
   componentWillUnmount() {
-    const mapElement = this._mapRef.current;
-    mapElement.remove();
+    this.mapObject.remove();
+    this.mapObject = null;
   }
 
   render() {
